9eea24efe94f87e0069e7c2047a308c42d3d5f38
Shun <shun12baseball@gmail.com>
2023-02-12 14:26:43 +0900

    feat: posts/aboutを作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import mdx from '@astrojs/mdx'
+import remarkToc from 'remark-toc'
+import rehypeMinifyHtml from 'rehype-preset-minify'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'one-dark-pro'
+      },
+      remarkPlugins: [remarkToc],
+      rehypePlugins: [rehypeMinifyHtml],
+      remarkRehype: {},
+      gfm: false
+    })
+  ]
+})
